63eb1549dd9f65ea9e075dcf768a8aa37524b1f6
KMS06-24 <asd>
2019-04-20 14:54:06 +0900

    Counter, Player 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,77 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
-const Player = (props) => (
-  <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={()=> props.removePlayer(props.id)}>x</button>
-    </span>
-    <span className="player-name">
-      {props.name}
-    </span>
-    <Counter />
-  </div>
-);
-
-/*const Counter = (props) => (
-  <div className="counter">
-    <button className="counter-action decrement">-</button>
-    <span className="counter-score">{props.score}</span>
-    <button className="counter-action increment">+</button>
-  </div>
-) ;*/
-
-class Counter extends React.Component{
-  /*state = {
-    score: 0
-  }*/
-  constructor(){
-    super();
-    this.state = {
-      score: 0,
-      a:10
-    }
-    //this.incrementScore = this.incrementScore.bind(this);
-  }
-
-  incrementScore = () => {
-    console.log('increment:', this);
-    //this.state.score +=1;
-    //1)setState()로 값을 변경해야만 UI렌더링이 일어난다.
-    //this.setState({score:this.state.score + 1})
-    //2)merge된다. =. 변경한 state만 기존값에 오버라이딩 된다.
-    //3)setState는 비동기로 일어난다.
-    this.setState(prevState => ({score:prevState.score + 1}));
-  }
-
-  decrementScore(){
-    this.setState(prevState => ({score:prevState.score -1}));
-  }
-  render(){
-    return(
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore.bind(this)}>-</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}>+</button>
-      </div>
-    );
-  }
-}
-
-
-
-/*const App = (props)=>(
-  <div className="scoreboard">
-    {/!*{title:'My Scoreboard, totlaPlayers:11*!/}
-    <Header title="My Scoreboard" totalPlayers={11}/>
-
-    {
-      props.initialPlayers.map(player => <Player key={player.id} name={player.name} score={player.score}/>)
-    }
-
-  </div>
-);*/
 
 class App extends React.Component {
   state = {
---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export class Counter extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      score: 0,
+      a:10
+    }
+  }
+
+  incrementScore = () => {
+    this.setState(prevState => ({score:prevState.score + 1}));
+  }
+
+  decrementScore(){
+    this.setState(prevState => ({score:prevState.score -1}));
+  }
+  render(){
+    return(
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore.bind(this)}>-</button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}>+</button>
+      </div>
+    );
+  }
+}
