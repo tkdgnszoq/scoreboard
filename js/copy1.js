466cdc15b3bf2b9385b7def5205aadaf42c23021
KMS06-24 <asd>
2019-04-27 10:04:47 +0900

    PureComponent 를 이용한 성능향상

--- js/copy1.js
+++ js/copy1.js
@@ -5,9 +5,15 @@ let book = {title: "인사이드 자바스크립트", price: 18000};
 
 // 2. book을 카피하되 deep copy를 수행한 후에 price를 30000으로 바꾸고 출력하라. 두개의 값이 달라야 한다.
 // 2-1 Object.assign() 사용하여 deep copy하기 //새로운 메모리 주소 할당
-let copyBook = Object.assign({}, book, {a:1});
-copyBook.price = 20000;
-console.log(book);
-console.log(copyBook);
+// let copyBook = Object.assign({}, book, {a:1});
+// copyBook.price = 20000;
+// console.log(book);
+// console.log(copyBook);
 
 // 2-2 es6 spread 연산자를 사용하여 deep copy 하기
+let copyBook2 = {...book, ...{a:1}, ...{a:123}}; //새로운 메모리 시작주소 할당
+//console.log('copyBook2', copyBook2);
+
+copyBook2.price = 20000;
+console.log(book);
+console.log(copyBook2);
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = (props) => {
-  return (
-    <div className="player">
+
+export class Player extends React.PureComponent{
+  render() {
+    console.log('rendered', this.props);
+    return (
+      <div className="player">
     <span className="player-name">
-      <button className="remove-player" onClick={()=> props.removePlayer(props.id)}>x</button>
+      <button className="remove-player" onClick={()=> this.props.removePlayer(this.props.id)}>x</button>
     </span>
-      <span className="player-name">
-      {props.name}
+        <span className="player-name">
+      {this.props.name}
     </span>
-      <Counter score={props.score} id={props.id} changeScore={props.changeScore}/>
-    </div>
-  );
+        <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+      </div>
+    );
+  }
 }
