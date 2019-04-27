a33a47b039b3ebe6c386229be99c87bcd73127ef
KMS06-24 <asd>
2019-04-27 17:00:36 +0900

    App 컴포넌트 : players를 state에서 store subscribe로 변경

--- src/App.js
+++ src/App.js
@@ -3,27 +3,11 @@ import './App.css';
 import Header from "./components/Header";
 import {Player} from "./components/Player";
 import {AddFormPlayer} from "./components/AddFormPlayer";
+import {connect} from "react-redux";
 
 
 class App extends React.Component {
-
   maxId = 4;
-
-  state = {
-    players:[
-      {name: 'LDK', score:0, id:1},
-      {name: 'HONG', score:0, id:2},
-      {name: 'KIM', score:0, id:3},
-      {name: 'PARK', score:0, id:4}
-    ]
-  }
-
-  /*handleRemovePlayer = (id) => {
-    debugger;
-    const players1 = this.state.players.filter(player => (player.id !== id));
-    this.setState({players: players1})
-    //this.setState({players1}) //short hand property 키,value같으면 하나만써도됨.
-  }*/
 
   handleRemovePlayer = (id) => {
     this.setState(prevState => {
@@ -56,10 +40,10 @@ class App extends React.Component {
     render(){
     return (
       <div className="scoreboard">
-        <Header players={this.state.players}/>
+        <Header players={this.props.players}/>
         {
           //props.initialPlayers.map(player => <Player key={player.id} name={player.name}/>)
-          this.state.players.map(player => <Player key={player.id}
+          this.props.players.map(player => <Player key={player.id}
                                                    id={player.id}
                                                    score={player.score}
                                                    removePlayer={this.handleRemovePlayer}
@@ -72,4 +56,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) =>({
+  players: state.playerReducer.players
+});
+
+export default connect(mapStateToProps)(App);
